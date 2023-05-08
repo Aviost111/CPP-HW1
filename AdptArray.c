f67af96ddf985c50329dd47e71b2146d7746dc8a
Aviost111 <Aviostroff@yahoo.com>
2023-05-08 19:14:36 +0300

    .

--- AdptArray.c
+++ AdptArray.c
@@ -27,14 +27,17 @@ PAdptArray CreateAdptArray(COPY_FUNC copy, DEL_FUNC del, PRINT_FUNC print) {
 }
 
 void DeleteAdptArray(PAdptArray arr) {
+    //if null return
     if (!arr) {
         return;
     }
+    //delete the content if its not null
     for (int i = 0; i < arr->ArrSize; i++) {
         if (arr->pEArr[i]) {
             arr->delFunc(arr->pEArr[i]);
         }
     }
+    //delete the array and then the struct
     free(arr->pEArr);
     free(arr);
 }
@@ -44,12 +47,15 @@ Result SetAdptArrayAt(PAdptArray arr, int index, PElement elem) {
     if (!arr) {
         return FAIL;
     }
+    //if array is too small we need to make it bigger
     if (arr->ArrSize <= index) {
+        //if it doesn't exist yet create it
         if (!arr->pEArr) {
             arr->pEArr = (PElement*)calloc((index + 1), sizeof(PElement));
             if(!arr->pEArr){
                 return FAIL;
             }
+            //else just make it bigger
         } else {
             newArr = (PElement*)calloc((index + 1), sizeof(PElement));
             if(!newArr) {
@@ -59,11 +65,16 @@ Result SetAdptArrayAt(PAdptArray arr, int index, PElement elem) {
             free(arr->pEArr);
             arr->pEArr = newArr;
         }
+        //add the element
         arr->pEArr[index] = arr->copyFunc(elem);
         arr->ArrSize = index + 1;
         return SUCCESS;
     } else {
-        arr->delFunc(arr->pEArr[index]);
+        //if it's not null delete it
+        if(arr->pEArr[index]) {
+            arr->delFunc(arr->pEArr[index]);
+        }
+        //add it
         arr->pEArr[index] = arr->copyFunc(elem);
         return SUCCESS;
     }
